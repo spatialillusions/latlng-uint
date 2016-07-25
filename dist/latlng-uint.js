1d36dff3540f23c4b39f4370a3d8445893b14232
Måns Beckman <mans.beckman@spatialillusions.com>
2016-07-25 17:54:32 +0200

    Lots of fixes for how they store negative values

--- dist/latlng-uint.js
+++ dist/latlng-uint.js
@@ -2,24 +2,30 @@
 function latlng2uint(latlng, precision){
 	var coordinates = [];
 	function int2uint(number, precision){
-        precision = Math.pow(2, precision) - 1;
-        return number & precision;
+        if(number<0){
+        	return Math.abs(number-1) | Math.pow(2,precision-1);
+        }else{
+        	return parseInt(number);
+        }
     }
-	coordinates[0] = int2uint((latlng[0] / 90) * (Math.pow(2,precision[0]-1)-1),precision[0]);
-	coordinates[1] = int2uint((-(latlng[1] + 180) / 180) * (Math.pow(2,precision[1]-1)-1),precision[1]);
+	coordinates[0] = int2uint((latlng[0] / 90) * (Math.pow(2,precision[0]-1)),precision[0]);
+	coordinates[1] = int2uint((latlng[1] / 180) * (Math.pow(2,precision[1]-1)),precision[1]);
 
 	return coordinates;	
 }
 
 function uint2latlng(coordinate, precision){
 	var latlng = [];
-	function uint2int(uint, precision){
-		uint <<= 32 - precision;
-    	uint >>= 32 - precision;
-    	return uint;
+	function uint2int(number, precision){
+		if(number > Math.pow(2,precision-1)){
+			return -(number-1) | Math.pow(2,precision-1);
+		}else{
+			return number;
+		}
+
 	}
-	latlng[0] = uint2int(coordinate[0],precision[0])/ (Math.pow(2,precision[0]-1)-1) * 90;
-	latlng[1] = -180 - (uint2int(coordinate[1],precision[1])/ (Math.pow(2,precision[1]-1)-1) * 180);
+	latlng[0] = uint2int(coordinate[0],precision[0]) * (90/(Math.pow(2,precision[0]-1)-1));
+	latlng[1] = uint2int(coordinate[1],precision[1]) * (180/(Math.pow(2,precision[1]-1)-1));
 	
 	return latlng;
 }
